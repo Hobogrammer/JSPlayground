853a42a327e45b9d32b094c606b2c77da31e55d4
Andre Smith <lordsilent@gmail.com>
2014-02-12 23:51:21 -0800

    Much closer, full functionality soon

--- book.js
+++ book.js
@@ -1,42 +1,34 @@
-var BookModule = (function()
-{ //JAVA HABITS
-  var pages = 0;
-  var title = "";
-  var author =  "";
-  var hardback = "";
-  var currentPage = 0;
+function Book(title,author,hardback,pages,currentPage){
+  this.title = title;
+  this.author = author;
+  this.hardback = hardback;
+  this.pages = pages;
+  this.currentPage = currentPage;
+}
+Book.prototype.toString = function () {
+  return console.log(this.pages + " page long book named \"" + this.title +
+      " \"written by: " + this.author + ", currently on page " + this.currentPage +
+      ". Hardback? " + this.hardback + ".")
+}
+var BookList = (function() {
+  var books = []
 
-  // BookModule.prototype.toString = function()
-  // {
-  //   return this.page + " page long book named " + this.title +
-  //    " written by: " + this.author + ", currently on page " + this.currentPage +
-  //    ". Hardback? " + this.hardback + ".";
-  // };
-
-  return{
-    setBookValues:function(pages, title, author, hardback, currentPage)
-    {
-      book.title = title;
-      book.author = author;
-      book.hardback = hardback;
-      book.pages = pages;
-      book.currentPage = currentPage;
+  return { //if this isn't here I get weird errors
+    addToList:function(book) {
+      books.push(book)
     },
-    getCurrentPage:function()
-    {
+    getCurrentPage:function(book) {
       return book.currentPage;
     },
-    incrementPage:function(book)
-    {
+    incrementPage:function(book) {
       book.currentPage += 1;
       return book.currentPage;
     },
-    decrementPage:function(book)
-    {
+    decrementPage:function(book) {
       book.currentPage -+ 1;
       return book.currentPage;
     }
   };
-})
-
-BookModule.setBookValues();
+})();
+myBook = new Book("Ginga Eiyuu Densetsu","Yoshiki Tanaka", true, 500, 51);
+BookList.addToList(myBook);
